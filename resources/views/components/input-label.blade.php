1e999a1e75955643ee857f454a8e0a59a5a748aa
Sayyed Aamir <mehmetvladimir@gmail.com>
2025-01-30 21:59:07 +0700

    minor change in components

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block text-sm font-medium text-gray-900 dark:text-gray-100']) }}>
+<label {{ $attributes->merge(['class' => 'mb-2 block text-sm font-medium text-gray-900 dark:text-gray-100']) }}>
     {{ $value ?? $slot }}
 </label>
