0283a06590ed8c05ef7d52512ccfd6d17be9b458
LinCie <aldiofernanda@protonmail.com>
2025-02-08 22:49:00 +0700

    feat: create purchase products table

---
+++ database/migrations/tenant/2025_02_08_154645_create_purchase_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchase_products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('purchase_id')->constrained();
+            $table->foreignId('product_id')->constrained();
+            $table->integer('quantity');
+            $table->decimal('buying_price', 10, 2)->nullable();
+            $table->decimal('total_cost', 10, 2)->nullable();
+            $table->string('notes')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchase_products');
+    }
+};
