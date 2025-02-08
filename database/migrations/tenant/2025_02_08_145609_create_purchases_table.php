07dd83fa5b5d5de4dd18561e40df859cea3c102c
LinCie <aldiofernanda@protonmail.com>
2025-02-08 22:32:39 +0700

    feat: add purchases table

---
+++ database/migrations/tenant/2025_02_08_145609_create_purchases_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('total_amount', 15, 2)->default(0);
+            $table->string('status');
+            $table->string('admin_notes')->nullable();
+            $table->string('supplier_notes')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreignId('warehouse_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
