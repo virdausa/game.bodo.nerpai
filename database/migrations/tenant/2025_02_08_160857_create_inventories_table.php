e1fa2dc2183f688d79b4c04977710e0d77e8f3b0
LinCie <aldiofernanda@protonmail.com>
2025-02-08 23:25:15 +0700

    feat: create inventories table

---
+++ database/migrations/tenant/2025_02_08_160857_create_inventories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            // Primary Key
+            $table->id();
+
+            // Foreign Keys
+            $table->foreignId('product_id')->constrained();
+            $table->foreignId('warehouse_id')->constrained();
+
+            // Columns
+            $table->date('expire_date')->nullable();
+            $table->integer('quantity')->default(0);
+            $table->integer('reserved_quantity')->default(0);
+            $table->integer('in_transit_quantity')->default(0);
+
+            // Timestamps (optional, not in schema but commonly used)
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('inventories');
+    }
+};
