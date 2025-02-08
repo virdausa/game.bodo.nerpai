93ba62301841aafc4340b9262daaf2fed9802737
LinCie <aldiofernanda@protonmail.com>
2025-02-08 22:00:56 +0700

    feat: add warehouses migration

---
+++ database/migrations/tenant/2025_02_08_144343_create_warehouses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('warehouses', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->json("address")->nullable();
+            $table->string("status");
+            $table->string("notes")->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('warehouses');
+    }
+};
