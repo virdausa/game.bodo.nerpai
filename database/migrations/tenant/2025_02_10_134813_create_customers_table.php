16f9f41e9ec57dfd035d6234df686a9f15a3eee5
LinCie <aldiofernanda@protonmail.com>
2025-02-10 21:05:43 +0700

    feat: create customers table

---
+++ database/migrations/tenant/2025_02_10_134813_create_customers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            // Primary Key
+            $table->id();
+
+            // Columns
+            $table->string('name');
+            $table->json('address')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('status');
+            $table->text("notes")->nullable();
+
+            // Timestamps
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
