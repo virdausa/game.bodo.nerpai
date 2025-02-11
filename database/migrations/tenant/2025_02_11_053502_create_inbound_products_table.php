25aa9a80aa9d0198c02eb2af5b9d8252a1cac84f
LinCie <aldiofernanda@protonmail.com>
2025-02-11 12:53:02 +0700

    feat: add soft deletes to inbound products

--- database/migrations/tenant/2025_02_11_053502_create_inbound_products_table.php
+++ database/migrations/tenant/2025_02_11_053502_create_inbound_products_table.php
@@ -26,6 +26,7 @@ return new class extends Migration
 
             // Timestamps (optional, not in ERD but commonly used)
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
