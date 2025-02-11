91d35eb67b6f0f91e0075304370b14056204e4a3
LinCie <aldiofernanda@protonmail.com>
2025-02-11 12:43:49 +0700

    feat: create inbound products table

---
+++ database/migrations/tenant/2025_02_11_053502_create_inbound_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inbound_products', function (Blueprint $table) {
+            // Primary key
+            $table->id();
+
+            // Foreign keys
+            $table->foreignId('inbound_id')->constrained();
+            $table->foreignId('product_id')->constrained();
+            // $table->foreignId('location_id')->constrained();
+
+            // Columns
+            $table->integer('quantity')->nullable();
+            $table->text('notes')->nullable();
+
+            // Timestamps (optional, not in ERD but commonly used)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inbound_products');
+    }
+};
