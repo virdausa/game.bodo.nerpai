270a2dfc1472aef0ba85150f335056e122c5f4d4
LinCie <aldiofernanda@protonmail.com>
2025-02-15 22:22:11 +0700

    fix: fix missing imports

--- database/seeders/WarehouseSeeder.php
+++ database/seeders/WarehouseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Location;
 use App\Models\Warehouse;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +16,11 @@ class WarehouseSeeder extends Seeder
         $warehouse1 = Warehouse::create([
             'name' => 'Warehouse 1',
             'address' => json_encode([
-                'street'=>'Jl. Contoh 1',
-                'city'=>'Contoh Kota',
-                'province'=>'Contoh Provinsi',
-                'country'=>'Contoh Negara',
-                'postal_code'=>'12345',
+                'street' => 'Jl. Contoh 1',
+                'city' => 'Contoh Kota',
+                'province' => 'Contoh Provinsi',
+                'country' => 'Contoh Negara',
+                'postal_code' => '12345',
             ]),
             'status' => 'Active',
             'notes' => 'Seeder',
@@ -28,28 +29,28 @@ class WarehouseSeeder extends Seeder
         $warehouse2 = Warehouse::create([
             'name' => 'Warehouse 2',
             'address' => json_encode([
-                'street'=>  'Jl. Contoh 2',
-                'city'=> 'Contoh Kota',
-                'province'=> 'Contoh Provinsi',
-                'country'=> 'Contoh Negara',
-                'postal_code'=> '12345',
+                'street' =>  'Jl. Contoh 2',
+                'city' => 'Contoh Kota',
+                'province' => 'Contoh Provinsi',
+                'country' => 'Contoh Negara',
+                'postal_code' => '12345',
             ]),
             'status' => 'Active',
             'notes' => 'Seeder',
         ]);
 
-        for($room = 1; $room <= 3; $room++) {
-            for($rack = 1; $rack <= 2; $rack++) {
+        for ($room = 1; $room <= 3; $room++) {
+            for ($rack = 1; $rack <= 2; $rack++) {
                 Location::create([
                     'warehouse_id' => $warehouse1->id,
-                    'room' => 'Room '.$room,
-                    'rack' => 'Rack '.$rack,
+                    'room' => 'Room ' . $room,
+                    'rack' => 'Rack ' . $rack,
                     'notes' => 'Seeder',
                 ]);
                 Location::create([
                     'warehouse_id' => $warehouse2->id,
-                    'room' => 'Room '.$room,
-                    'rack' => 'Rack '.$rack,
+                    'room' => 'Room ' . $room,
+                    'rack' => 'Rack ' . $rack,
                     'notes' => 'Seeder',
                 ]);
             }
