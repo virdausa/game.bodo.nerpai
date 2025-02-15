010f7521649c70ccbde66124c3c0e05b8174216f
LinCie <aldiofernanda@protonmail.com>
2025-02-15 17:22:01 +0700

    fix: remove undefined permissionseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            PermissionSeeder::class,
+            // TODO: Create permission seeder
+            // PermissionSeeder::class,
         ]);
     }
 }
