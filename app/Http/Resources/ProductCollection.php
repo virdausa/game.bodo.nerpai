88d37a5c834e1ae5a0acc6d2b1996a303c913c63
LinCie <aldiofernanda@protonmail.com>
2025-02-17 00:10:32 +0700

    fix: fix static error

--- app/Http/Resources/ProductCollection.php
+++ app/Http/Resources/ProductCollection.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class ProductCollection extends ResourceCollection
 {
-    public $wrap = '';
+    public static $wrap = '';
 
     public function toArray(Request $request): array
     {
